0de0019bd04efd55569d8b2e170ed8a7eb4883e0
viktor111 <swifrorlilko@gmail.com>
2020-04-29 17:56:40 +0300

    add validations

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -1,5 +1,6 @@
 const AuthService = require('../Services/Auth')
 const User = require('../Models/User')
+const DbContext = require('../Config/dbContext')
 
 const GetRegister = (req, res) => {
     res.render("Auth/Register");
@@ -9,14 +10,27 @@ const GetLogin = (req, res) => {
     res.render("Auth/Login");
 }
 
-const PostRegister = (req, res) => {
-    let {Username, Email, Password} = req.body;
+const  PostRegister = async (req, res) => {
+
+    let authService = new AuthService();
+
+    let { Username, Email, Password } = req.body;
     console.log(`${Username} , ${Email} , ${Password}`);
-    
-    let service = new AuthService();
-    let user = new User(Username, Email, Password, new Date());
-    service.SaveUser(user);
-    res.redirect("/");
+    await authService.EmailExist(Email, res);
+
+    // if(authService.EmailExist(Email)){
+    //     res.render("Auth/Register", { error: "Email already exists!" }) 
+    // }
+    if (Password.length < 4) {
+        res.render("Auth/Register", { error: "Password too small!" })
+    }
+
+    else {
+
+        let user = new User(Username, Email, Password, new Date());
+        authService.SaveUser(user);
+        res.redirect("/");
+    }
 }
 
 module.exports = {
--- Services/Auth.js
+++ Services/Auth.js
@@ -1,7 +1,7 @@
 const DbContext = require('../Config/dbContext')
 
-class Auth{
-    SaveUser(User){
+class Auth {
+    SaveUser(User) {
         let dbContext = new DbContext().Initialize("users");
 
         dbContext.add({
@@ -9,7 +9,28 @@ class Auth{
             email: User.email,
             password: User.password,
             created: User.created
-        })        
+        })
+    }
+
+      async EmailExist(email, res) {
+        let dbContext = new DbContext().Initialize("users");
+
+        let toReturn =false;
+        let counter = 0;
+        let promise = dbContext.where("email", "==", email).limit(1).get()
+        .then((user) => {
+            user.forEach(() => {
+                counter++;
+            })
+        })
+        .finally(() => {
+            console.log(counter);
+
+            if(counter !== 0){
+                return res.render("Auth/Register", {error: "Email already exists!"})            
+            }
+        });
+
     }
 }
 
