55c130ca582d4ca2b22e312fce873d1f200372cc
viktor111 <swifrorlilko@gmail.com>
2020-05-07 16:39:36 +0300

    add random key generator

--- Services/Auth.js
+++ Services/Auth.js
@@ -1,5 +1,6 @@
 const DbContext = require('../Config/dbContext');
 const Mailer = require("../Helpers/Mailer");
+const KeyGenerate = require("../Helpers/RandomNum")
 const jwt = require("jsonwebtoken");
 const bcrypt = require('bcrypt');
 
@@ -9,8 +10,10 @@ class Auth {
     SaveUser(User) {
 
         let mailer = new Mailer();
-        mailer.SendEmail(User.email, "test", "subject test");
-        
+        mailer.SendEmail(User.email, "<h1>test</h1>", "subject test");
+
+        let randomKey = KeyGenerate(9, 0);
+
         let dbContext = new DbContext().Initialize("users");
         
         bcrypt.hash(User.password, 12, (err, hash) => {
@@ -22,7 +25,8 @@ class Auth {
                 email: User.email,
                 password: hash.toString(),
                 created: User.created,
-                isAdmin: true
+                isAdmin: true,
+                key: randomKey
             })
         })
       
