272eb915283fcf006eb740d3de6f8e46be30badb
viktor111 <swifrorlilko@gmail.com>
2020-05-14 22:56:03 +0300

    adding post create project

--- Controllers/ProjectController.js
+++ Controllers/ProjectController.js
@@ -4,11 +4,13 @@ const GetProjects = (req, res) => {
     const token = req.cookies.token;
     if (!token){
         res.render("Project/Main", { logged: false})
+        res.end()
 
     }
     else {
         let payload = jwt.verify(token, "auth")
         res.render("Project/Main", {title: "Create Project", logged: true, username: payload.username, admin: payload.admin});
+        res.end()
     }
 }
 
@@ -16,11 +18,24 @@ const CreateProject = (req, res) => {
     const token = req.cookies.token;
     if (!token){
         res.render("Auth/Register", { error: "You need to have an account!" })
+        res.end()
 
     }
     else {
         let payload = jwt.verify(token, "auth")
         res.render("Project/Create", {title: "Create Project", logged: true, username: payload.username, admin: payload.admin});
+        res.end()
+    }
+}
+
+const PostProject = (req, res) => {
+    const token = req.cookies.token
+    if (!token) {
+        res.render("Auth/Register", { error: "You need to have account!" })
+        res.end()
+    }
+    else {
+       const {Title, Description, GitHub} 
     }
 }
 
